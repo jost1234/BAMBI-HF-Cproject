6e08f67227b7476482ee3a4d1cc7ed99d64e921f
Márk Benedek Jost <jost.bence@gmail.com>
2022-10-30 15:16:58 +0100

    rákos github

--- KacsaC/src/kijelzo.h
+++ KacsaC/src/kijelzo.h
@@ -23,14 +23,14 @@
     void kacsaRender(uint8_t kacsaPoz);
 
 // Megjelenit egy lovedeket
-    void lovedekRender(uint8_t lokedekPoz, bool lovedekMagassag);
+    void lovedekRender(lov_type lovedek);
 
 
 // Egy kepkocka megjelenitese jatek kozben, egyeb funkcionalitasra nem alkalmas (minel gyorsabban lefusson, majd masik fv intezi pl villogast)
     // Kell hozza: jatekos pozicioja, kacsa pozicioja, lovedek(ek) pozicioja, lovedek magassaga: false alacsony, true magas
 	// Ha eltalaltak kacsat:
 	// 2-szer villog, ez ugy tortenjen hogy egy szamlalo 0 ha elozo kepen magasan volt alatta egy lovedek
-void render(uint8_t jatekosPoz, uint8_t kacsaPoz, bool eltalaltKacsa,
-		    uint8_t haldoklasi_fazis, uint8_t lovedekszam, uint8_t lokedekIdx[7],
-			bool lovedekMagassag[7], uint8_t osszkacsa, uint8_t lelottkacsa);
+void render(uint8_t jatekosPoz,uint8_t kacsaPoz, bool eltalaltKacsa,
+		uint8_t haldoklasi_fazis, lov_type plovedek,
+		uint8_t posszkacsa, uint8_t plelottkacsa);
 
